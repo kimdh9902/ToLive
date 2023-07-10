a62c99cb9849cf2297504b8f410775705c2834ac
jjam0527 <jjam0527@gmail.com>
2023-07-10 17:52:41 +0900

    sample-2

--- src/main/java/com/spring/domain/SearchIdVO.java
+++ src/main/java/com/spring/domain/SearchIdVO.java
@@ -1,5 +1,14 @@
 package com.spring.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class SearchIdVO {
+	private String id;
+	private String name;
 
 }
--- src/test/java/com/spring/mapper/testMapper.java
+++ src/test/java/com/spring/mapper/testMapper.java
@@ -2,18 +2,37 @@ package com.spring.mapper;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.spring.domain.SearchIdVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
 public class testMapper {
-
+	
+	@Setter (onMethod_ = @Autowired)
+	private SearchMapper mapper;
+	
 	@After
 	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		List<SearchIdVO> list = mapper.selectId("김옥수");
+		assertNotNull(list);
+		log.info(list);
 	}
 
 }
