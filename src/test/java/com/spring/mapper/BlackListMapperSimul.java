477b6b33419ded62514e2bb9360376bff7f14956
kimdong070 <kimdh990226@naver.com>
2023-07-10 20:53:26 +0900

    집 가 자

--- src/test/java/com/spring/mapper/BlackListMapperSimul.java
+++ src/test/java/com/spring/mapper/BlackListMapperSimul.java
@@ -2,12 +2,16 @@ package com.spring.mapper;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.domain.BlackListVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -20,20 +24,25 @@ public class BlackListMapperSimul {
 	@Setter(onMethod_ = @Autowired)
 	private BlackListMapper mapper;
 	
-	@Test
+	@Test @Ignore
 	public void testselectAllBlackList() {
-		assertNotNull(mapper.getClass().getName());
-		log.info(mapper.getClass().getName());
+		List<BlackListVO> list = mapper.selectAllBlackList("user02");
+		assertNotNull(list);
 	}
 	
 	@Test @Ignore
 	public void testinsertBlackList() {
-		
+		mapper.insertBlackList("user02", "celeb01");
+		log.info("###########돌긴했음########");
+		List<BlackListVO> list = mapper.selectAllBlackList("user02");
+		assertNotNull(list);
 	}
 	
 	@Test @Ignore
 	public void testdeleteBlackList() {
-		
+		mapper.deleteBlackList("user02", "celeb01");
+		log.info("###########돌긴했음########");
+		assertNotNull(mapper.selectAllBlackList("user02"));
 	}
 
 }
