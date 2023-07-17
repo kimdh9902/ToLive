c3a7fc844fa4b07d01a02be2b391267091c52854
jjam0527 <jjam0527@gmail.com>
2023-07-17 11:11:27 +0900

    start 07 17

--- src/main/java/com/spring/object/MyFile.java
+++ src/main/java/com/spring/object/MyFile.java
@@ -10,7 +10,7 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @NoArgsConstructor
 public class MyFile {
-	private String defaultFolder = "C:\\spring-tool-suite-3.9.18\\TripToLive";
+	private String defaultFolder = "C:\\spring-tool-suite-3.9.18\\workspace\\TripToLive\\src\\main\\webapp\\resources\\TripToLive";
 	private String uploadFolder = "";
 	private String fileName;
 
@@ -56,6 +56,27 @@ public class MyFile {
 	     return true;
 	}
 
-	
+	public boolean uploadFile(MultipartFile uploadFile) {
+	     if(uploadFile != null) {
+	        log.info("upload File Name : " + uploadFile.getOriginalFilename() );
+	        log.info("upload File Size : " + uploadFile.getSize() );
+//	        File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+	        // multipartFile.getOriginalFilename() 클라이언트의 파일 시스템에서 원래 파일 이름을 반환합니다.
+	        // File(File parent, String child) 폴더 밑에 자식
+	        this.setFileName(this.getFileName()+"_"+uploadFile.getOriginalFilename());
+	        File saveFile = new File(this.getUploadFolder(), this.getFileName());
+	        if(!saveFile.exists()) {
+	        	saveFile.getParentFile().mkdirs();
+	        }
+	        try {
+	        	uploadFile.transferTo(saveFile); // 수신된 파일을 지정된 대상 파일로 전송
+	        } catch (Exception e) {
+	            log.error(e.getMessage());
+	        	e.printStackTrace();
+	           return false;
+	        }
+	     }
+	     return true;
+	}
 	 
 }
