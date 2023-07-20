560481e220d5ba4f8ae9933543b361401c33db5c
kimdong070 <kimdh990226@naver.com>
2023-07-20 09:05:04 +0900

    check3

--- src/main/java/com/spring/mapper/EmojiMapper.java
+++ src/main/java/com/spring/mapper/EmojiMapper.java
@@ -3,6 +3,7 @@ package com.spring.mapper;
 import com.spring.domain.EmojiVO;
 
 public interface EmojiMapper {
+	public Integer selectEmoji(EmojiVO vo);
 	public void insertEmoji(EmojiVO vo);
 	public void updateEmoji(EmojiVO vo);
 }
